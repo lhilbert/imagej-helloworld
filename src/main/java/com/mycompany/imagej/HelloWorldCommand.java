8ace21202f487fc8607bf310c216fb844d9d2812
lhilbert <hilbert@lhilbert-private-2.local>
2018-06-04 14:40:07 +0200

    Add initial files
    Added the files needs for the Hello World ImageJ plugin from 2018
    Learnathon

---
+++ src/main/java/com/mycompany/imagej/HelloWorldCommand.java
@@ -0,0 +1,78 @@
+/*
+ * To the extent possible under law, the ImageJ developers have waived
+ * all copyright and related or neighboring rights to this tutorial code.
+ *
+ * See the CC0 1.0 Universal license for details:
+ *     http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+package com.mycompany.imagej;
+
+import net.imagej.Dataset;
+import net.imagej.ImageJ;
+import net.imagej.ops.OpService;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.Img;
+import net.imglib2.type.numeric.RealType;
+
+import org.scijava.ItemIO;
+import org.scijava.command.Command;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.ui.UIService;
+
+import bsh.Console;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This example illustrates how to create an ImageJ {@link Command} plugin.
+ * <p>
+ * The code here is a simple Gaussian blur using ImageJ Ops.
+ * </p>
+ * <p>
+ * You should replace the parameter fields with your own inputs and outputs,
+ * and replace the {@link run} method implementation with your own logic.
+ * </p>
+ */
+@Plugin(type = Command.class, menuPath = "Plugins>Hello World")
+public class HelloWorldCommand<T extends RealType<T>> implements Command {
+    //
+    // Feel free to add more parameters here...
+    //
+
+	@Parameter
+	private UIService myConsole;
+	
+	@Parameter(type=ItemIO.OUTPUT)
+	private String outString;
+	
+    @Override
+    public void run() {
+        
+    	outString = "Hello World!";
+//    	myConsole.showDialog(outString);
+//
+//    	System.out.println("Hello World!");
+
+    	
+    }
+
+    /**
+     * This main function serves for development purposes.
+     * It allows you to run the plugin immediately out of
+     * your integrated development environment (IDE).
+     *
+     * @param args whatever, it's ignored
+     * @throws Exception
+     */
+    public static void main(final String... args) throws Exception {
+        // create the ImageJ application context with all available services
+        final ImageJ ij = new ImageJ();
+        ij.ui().showUI();
+        
+    }
+
+}
